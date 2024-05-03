40377794d9342ae045041324b7e0d73ce8575c15
Gust4voSSM <gugu9santiago@gmail.com>
2024-05-03 16:36:28 -0300

    Added air acceleration and drag; Walljumps now thorw the character away from the wall

--- Player.gd
+++ Player.gd
@@ -1,66 +1,96 @@
 extends CharacterBody2D
  
-const SPEED = 200.0
-const JUMP_VELOCITY = -500.0
+const SPEED = 300.0
+const MAX_SPEED = 500.0
+const AIR_ACCELERATION = 2000.0
+const JUMP_VELOCITY = -700.0
+const DRAG = 0.8 # Value must be between 0 and 1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var doubleJump = false
 var is_hiding = false
+var is_facing_right = true
 
 func _physics_process(delta):
-	jump(delta)
+	var sprite = $AnimatedSprite2D
 	
 	# Hide Action
 	if Input.is_action_just_pressed("hide") and is_on_floor() and !is_hiding:
-		$AnimatedSprite2D.play("hide")
+		sprite.play("hide")
 		is_hiding = true
+		
 	# Pop Action (Unhide)
 	if Input.is_action_just_pressed("pop") and is_hiding:
-		$AnimatedSprite2D.play("pop")
+		sprite.play("pop")
 		is_hiding = false
-
-	# Movement Action
-	# First get the axis of movement, negative for left, positive for right
-	var direction = Input.get_axis("move_left", "move_right")
-	# If it is hiding it cannot move
-	if direction and !is_hiding:
-		$AnimatedSprite2D.play("walk")
-		if !$AnimatedSprite2D.is_flipped_h() and direction < 0:
-			$AnimatedSprite2D.set_flip_h(true)
-		elif $AnimatedSprite2D.is_flipped_h() and direction > 0:
-			$AnimatedSprite2D.set_flip_h(false)
-		
-		velocity.x = direction * SPEED
-		# Handling running
-		if Input.is_key_pressed(KEY_SHIFT):
-			velocity.x = direction * SPEED * 1.5
-	else: # Stops walking animation if not moving
-		if $AnimatedSprite2D.animation == "walk":
-			$AnimatedSprite2D.stop()
-		velocity.x = 0 #move_toward(velocity.x, 0, SPEED)
 	
 	# Idle animations normally and when hiding
 	if velocity.x == 0 and velocity.y == 0 and !$AnimatedSprite2D.is_playing():
 		if !is_hiding: $AnimatedSprite2D.play("idle")
 		if is_hiding: $AnimatedSprite2D.play("peek")	
-		
-	move_and_slide()
-
-func jump(delta):
-	# Gravity and Wall Sliding y velocity
+			 
+	# Gravity
 	velocity.y += gravity * delta
-	if is_on_floor(): velocity.y = 0
+	if is_on_floor():
+		velocity.y = 0
+		
+	# Wall Sliding
 	elif is_on_wall_only() and Input.is_action_pressed("move"): 
 		velocity.y += (0.3 * gravity * delta)
 		velocity.y = min(velocity.y, 0.1 * gravity)
-	
+	move(delta)
 	if Input.is_action_just_pressed("jump"):
-		if is_on_floor(): # Jumping from the floor
-			velocity.y = JUMP_VELOCITY
-			doubleJump = true
-		if !is_on_floor() and doubleJump: # Jumping in the air
-			velocity.y = JUMP_VELOCITY
-			doubleJump = false
-		if is_on_wall_only() and Input.is_action_pressed("move"): # Wall jumping
-			velocity.y = JUMP_VELOCITY
+		jump()
+	move_and_slide()
+	
+	
+	
+func move(delta):
+	if is_hiding: return
+	
+	var direction = get_x_direction()
+	var sprite = $AnimatedSprite2D
+	var flipped = sprite.is_flipped_h()
+	var sprite_direction = 1 if !flipped else -1
+	
+	if direction: sprite.play("walk")
+	
+	if direction != 0 and sprite_direction != direction:
+		sprite.set_flip_h(!flipped)
+		is_facing_right = !is_facing_right
+		
+	if is_on_floor():
+		velocity.x = direction * SPEED
+		velocity.x *= 2 if Input.is_key_pressed(KEY_SHIFT) else 1
+	else:
+		velocity.x *=  (1 - DRAG) ** delta
+		velocity.x = velocity.x + direction * AIR_ACCELERATION * delta
+		velocity.x = min(max(-MAX_SPEED, velocity.x), MAX_SPEED)
+		 
+		
+func get_x_direction():
+	return Input.get_axis("move_left", "move_right")
+
+func jump():
+	# Jumping from the floor		
+	if is_on_floor():
+		doubleJump = true
+	# Wall jumping
+	elif is_on_wall(): 
+		velocity.x = -MAX_SPEED if is_facing_right else MAX_SPEED
+		print(velocity.x)
+		$AnimatedSprite2D.set_flip_h(!$AnimatedSprite2D.is_flipped_h())
+		is_facing_right = !is_facing_right
+	# Jumping in the air	
+	elif doubleJump: 
+		doubleJump = false
+	# No jump	
+	else: return
+	
+	print("a")
+	velocity.y = JUMP_VELOCITY
+		
+		
+			
+

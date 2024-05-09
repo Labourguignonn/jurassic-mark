0d1d651b3a1304cbb68739cd4d2e33ec312af526
Lucas Araujo Bourguignon <labourguginon@gmail.com>
2024-05-09 19:41:36 -0300

    images

--- Player.gd
+++ Player.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
  
 const SPEED = 300.0
-const CROUCH_SPEED = 150.0
 const MAX_SPEED = 500.0
 const AIR_ACCELERATION = 2000.0
 const JUMP_VELOCITY = -700.0
@@ -12,57 +11,38 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var doubleJump = false
 var is_hiding = false
 var is_facing_right = true
-var can_move = true
+var is_moving = false
 var animation_timer = 0.0
+
 
 func _physics_process(delta):
 	var sprite = $AnimatedSprite2D
 	
+	gravityForce(delta)
+	move(delta)
+	
 	# Hide Action
 	if Input.is_action_just_pressed("hide") and is_on_floor() and !is_hiding:
-		sprite.play("hide")
-		is_hiding = true
-		can_move = true
+		is_moving = false
+		hiding(sprite,delta)
 		
 	# Pop Action (Unhide)
-	if (Input.is_action_just_pressed("pop") or !is_on_floor()) and is_hiding:
-		if can_stand():
-			sprite.play("pop")
-			print("tchau")
-			is_hiding = false
-			$CrouchHitBox.disabled = true
-			$StandingHitBox.disabled = false
-		#animation_timer = 0.3
-		
-	# Idle animations normally and when hiding
-	if velocity.x == 0 and velocity.y == 0 and !$AnimatedSprite2D.is_playing():
-		if !is_hiding: $AnimatedSprite2D.play("idle")
-		if is_hiding: $AnimatedSprite2D.play("peek")	
-			 
-	# Gravity
-	velocity.y += gravity * delta
-	if is_on_floor():
-		velocity.y = 0
-		doubleJump = true
-		
-	# Wall Sliding
-	elif is_on_wall_only() and Input.is_action_pressed("move"): 
-		velocity.y += (0.3 * gravity * delta)
-		velocity.y = min(velocity.y, 0.1 * gravity)
-		
-	if can_move:
-		move(delta)
+	if Input.is_action_just_pressed("pop") and is_hiding:
+		is_moving = false
+		pop(sprite,delta)
 	
-	### Deprecated, will never excecute, as can_move is constant true
-	elif is_hiding:
-		apply_drag(delta)
-	elif animation_timer > 0.0:
-		animation_timer -= delta
-	else:
-		can_move = true
-
+	# Jump Action 	
 	if Input.is_action_just_pressed("jump"):
-		jump()
+		jump(sprite)	
+	
+	# Idle Action
+	if !is_hiding and !is_moving:
+		sprite.play("idle")
+	
+	# Peek Action
+	if !$AnimatedSprite2D.is_playing() and is_hiding:
+		$AnimatedSprite2D.play("peek")
+	
 	move_and_slide()
 	
 func move(delta):
@@ -77,43 +57,52 @@ func move(delta):
 		$CrouchHitBox.disabled = false
 	
 	if direction: 
+		is_moving = true
 		if !is_hiding:
 			sprite.play("walk")
 		else:
 			# Implement crouch animation
 			pass
-			
-	if direction != 0 and sprite_direction != direction:
-		sprite.set_flip_h(!flipped)
-		is_facing_right = !is_facing_right
-		
-	if is_on_floor():
-		if !is_hiding:
-				velocity.x = direction * SPEED
-				velocity.x *= 2 if Input.is_key_pressed(KEY_SHIFT) else 1
-		else:
-			velocity.x = direction * CROUCH_SPEED	
-		
+		#correct sprite
+		if direction != 0 and sprite_direction != direction:
+			sprite.set_flip_h(!flipped)
+			is_facing_right = !is_facing_right	
+	else:
+		is_moving = false
+	
+	if is_on_floor():	
+		velocity.x = direction * SPEED
+		velocity.x *= 2 if Input.is_key_pressed(KEY_SHIFT) and !is_hiding else 1
 	else:
 		apply_drag(delta)
 		velocity.x = velocity.x + direction * AIR_ACCELERATION * delta
 		velocity.x = min(max(-MAX_SPEED, velocity.x), MAX_SPEED)
-		 
+
+func gravityForce(delta):
+	velocity.y += gravity * delta
+	if is_on_floor():
+		velocity.y = 0
+		doubleJump = true
 		
+	# Wall Sliding
+	elif is_on_wall_only() and Input.is_action_pressed("move"): 
+		velocity.y += (0.3 * gravity * delta)
+		velocity.y = min(velocity.y, 0.1 * gravity)
+	
 func get_x_direction():
 	return Input.get_axis("move_left", "move_right")
 
 func apply_drag(delta):
 	velocity.x *= (1 - DRAG) ** delta
 
-func jump():
+func jump(sprite):
 	# Jumping from the floor		
 	if is_on_floor():
 		doubleJump = true
 	# Wall jumping
 	elif is_on_wall(): 
 		velocity.x = -MAX_SPEED if is_facing_right else MAX_SPEED
-		$AnimatedSprite2D.set_flip_h(!$AnimatedSprite2D.is_flipped_h())
+		sprite.set_flip_h(!sprite.is_flipped_h())
 		is_facing_right = !is_facing_right
 	# Jumping in the air	
 	elif doubleJump: 
@@ -123,8 +112,17 @@ func jump():
 	
 	velocity.y = JUMP_VELOCITY
 
-func can_stand() -> bool:
-	var result = !$CrouchRaycast1.is_colliding() and !$CrouchRaycast2.is_colliding()	
-	return result	
-			
+func hiding(sprite,delta):
+	sprite.play("hide")
+	is_hiding = true		
 
+func pop(sprite,delta):
+	sprite.play("pop")
+	await get_tree().create_timer(delta * 8).timeout
+	is_hiding = false
+	$CrouchHitBox.disabled = true
+	$StandingHitBox.disabled = false
+
+#func waitAnimation(delta):
+	#var animSpeed = $AnimatedSprite2D.sprite_frames.get_animation_speed($AnimatedSprite2D.animation)
+	#await get_tree().create_timer(delta * 8).timeout

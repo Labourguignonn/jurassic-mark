1ec1589e59cbfb1b0e5e6a9e2edc0a917678a455
Pedro Campelo Lins <pedro.fev03@gmail.com>
2024-05-21 17:03:25 -0300

    Wall Jump Fix
    Character slides down walls without needing to be pressing buttons. Character can't move while in wall jump animation so that it can't climb a single wall. Character can't double jump from a wall. Added a tolerance timer for the character to jump right after starting to fall off a ledge or when turning against a wall.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -9,40 +9,36 @@ const DRAG = 0.8 # Value must be between 0 and 1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var doubleJump = false
-var is_crouching = false
-var is_facing_right = true
-var is_moving = false
-var is_dashing = false
-var can_dash = true
-var animation_timer = 0.0
+var can_have_tolerance_timer = false
+var double_jump 	= false
+var wall_jump 		= false
+var is_crouching 	= false
+var is_dashing 		= false
+var can_dash 		= true
 
 func _physics_process(delta):
 	var sprite = $AnimatedSprite2D
-	
-	if !is_dashing: 
-		move(delta)
+	if !is_dashing:
+		if $AnimationTimer.is_stopped(): move(sprite, delta)
 		gravityForce(delta)
 	
 	# Hide Action
 	if Input.is_action_just_pressed("hide") and is_on_floor() and !is_crouching:
-		is_moving = false
-		crouch(sprite,delta)
-		
+		crouch(sprite, delta)
+	
 	# Pop Action (Unhide)
-	if Input.is_action_just_pressed("pop") and is_crouching:
-		is_moving = false
+	if Input.is_action_just_pressed("pop") and is_crouching and !$CrouchRayCast.is_colliding():
 		pop(sprite,delta)
 	
-	# Jump Action 	
+	# Jump Action 
 	if Input.is_action_just_pressed("jump") and !is_crouching:
-		jump(sprite)	
-		
+		jump(sprite)
+	
 	if Input.is_action_just_released("jump"):
-		stop_jump(sprite)	
-		
+		stop_jump()
+	
 	# Idle Action
-	if !is_crouching and !is_moving:
+	if !is_moving() and !is_crouching:
 		sprite.play("idle")
 	
 	# Peek Action
@@ -53,11 +49,37 @@ func _physics_process(delta):
 		dash()
 	
 	move_and_slide()
+
+func gravityForce(delta):
+	if velocity.y < 2500:
+		velocity.y += gravity * delta
 	
-func move(delta):
-	
+	if is_on_floor():
+		can_have_tolerance_timer = true
+		double_jump = true
+		velocity.y = 0
+	# Wall Sliding
+	elif is_on_wall():
+		can_have_tolerance_timer = true
+		wall_jump = true
+		#velocity.y += (0.05 * gravity * delta)
+		velocity.y = min(velocity.y, 0.05 * gravity)
+	# Tolerance time for jumping right after starting to fall
+	elif can_have_tolerance_timer:
+		$ToleranceTimer.start()
+		can_have_tolerance_timer = false
+
+func get_x_direction():
+	return Input.get_axis("move_left", "move_right")
+
+func apply_drag(delta):
+	velocity.x *= (1 - DRAG) ** delta
+
+func is_moving():
+	return (velocity.x != 0 or velocity.y != 0)
+
+func move(sprite, delta):
 	var direction = get_x_direction()
-	var sprite = $AnimatedSprite2D
 	var flipped = sprite.is_flipped_h()
 	var sprite_direction = 1 if !flipped else -1
 	
@@ -66,7 +88,6 @@ func move(delta):
 		$CrouchHitBox.disabled = false
 	
 	if direction: 
-		is_moving = true
 		if !is_crouching:
 			sprite.play("walk")
 		else:
@@ -74,56 +95,39 @@ func move(delta):
 			pass
 		# Flip character sprite
 		if sprite_direction != direction:
-			sprite.set_flip_h(!flipped)
-			is_facing_right = !is_facing_right	
-	else:
-		is_moving = false
+			sprite.set_flip_h(!flipped)	
 	
-	if is_on_floor():	
-		velocity.x =  (direction * SPEED)/2 if is_crouching else direction * SPEED
-		velocity.x *= 2 if Input.is_key_pressed(KEY_SHIFT) and !is_crouching else 1
+	if is_on_floor():
+		velocity.x = (direction * SPEED)/2 if is_crouching else direction * SPEED
 	else:
 		apply_drag(delta)
 		velocity.x = velocity.x + direction * AIR_ACCELERATION * delta
 		velocity.x = min(max(-MAX_SPEED, velocity.x), MAX_SPEED)
 
-func gravityForce(delta):
-	if velocity.y < 2500:
-		velocity.y += gravity * delta
-	if is_on_floor():
-		velocity.y = 0
-		doubleJump = true
-		
-	# Wall Sliding
-	elif is_on_wall_only() and Input.is_action_pressed("move"): 
-		velocity.y += (0.3 * gravity * delta)
-		velocity.y = min(velocity.y, 0.1 * gravity)
-	
-func get_x_direction():
-	return Input.get_axis("move_left", "move_right")
-
-func apply_drag(delta):
-	velocity.x *= (1 - DRAG) ** delta
-
 func jump(sprite):
-	# Jumping from the floor		
-	if is_on_floor():
-		doubleJump = true
-	# Wall jumping
-	elif is_on_wall(): 
-		velocity.x = -MAX_SPEED if is_facing_right else MAX_SPEED
-		velocity.y = JUMP_VELOCITY/2
-		sprite.set_flip_h(!sprite.is_flipped_h())
-		is_facing_right = !is_facing_right
-	# Jumping in the air	
-	elif doubleJump: 
-		doubleJump = false
-	# No jump	
-	else: return
+	# Can jump if any of these conditions are satisfied in this specific order:
+	# is on floor? >>> is on wall? >>> is in air and tolerance timer is running? 
+	# >>> is in air and has double jump? If all fail, you can't jump.
+	var sprite_direction: int
 	
+	if is_on_floor(): pass
+	elif wall_jump:
+		double_jump = false
+		if $LeftRayCast.is_colliding(): 
+			sprite_direction = 1
+			sprite.set_flip_h(false)
+		if $RightRayCast.is_colliding(): 
+			sprite_direction = -1
+			sprite.set_flip_h(true)
+		velocity.x = sprite_direction * MAX_SPEED
+		$AnimationTimer.start()
+		print(double_jump)
+	elif $ToleranceTimer.get_time_left() > 0 and !can_have_tolerance_timer: pass
+	elif double_jump: double_jump = false
+	else: return
 	velocity.y = JUMP_VELOCITY
 
-func stop_jump(sprite):
+func stop_jump():
 	if velocity.y <= -200:
 		velocity.y = -200
 	
@@ -131,7 +135,7 @@ func crouch(sprite,delta):
 	apply_drag(delta)
 	velocity.x = max(SPEED/2,velocity.x)
 	sprite.play("hide")
-	is_crouching = true		
+	is_crouching = true
 
 func pop(sprite,delta):
 	sprite.play("pop")
@@ -144,18 +148,23 @@ func dash():
 	is_dashing = true
 	can_dash = false
 	velocity.x += get_x_direction() * DASH_SPEED
-	$DashTimer.start()
+	$AnimationTimer.start()
 	$DashCooldown.start()
 
-func _on_dash_timer_timeout():
-	is_dashing = false
+func _on_animation_timer_timeout():
+	if is_dashing: is_dashing = false
+	if wall_jump: wall_jump = false
 
 func _on_dash_cooldown_timeout():
 	if is_on_floor(): can_dash = true
 
-func has_moved():
-	return (velocity.x != 0 or velocity.y != 0)
+func _on_tolerance_timer_timeout():
+	if wall_jump and $AnimationTimer.is_stopped(): wall_jump = false 
 
-#func waitAnimation(delta):
-	#var animSpeed = $AnimatedSprite2D.sprite_frames.get_animation_speed($AnimatedSprite2D.animation)
-	#await get_tree().create_timer(delta * 8).timeout
+#func create_timer(wait_time:float, one_shot:bool = true, auto_start:bool = false):
+	#var timer = Timer.new()
+	#timer.set_wait_time(wait_time)
+	#timer.set_one_shot(one_shot)
+	#timer.set_autostart(auto_start)
+	#timer.timeout.connect(Callable(self, "_on_dash_timer_timeout"))
+	#return timer
